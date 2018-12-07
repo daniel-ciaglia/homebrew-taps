c89f00cc14b8d55dc25ea1672515bb635edfc571
Daniel Ciaglia <daniel.ciaglia@kreuzwerker.de>
2018-12-07 16:32:19 +0100

    prep cf-doc 0.0.2

---
+++ cf-doc.rb
@@ -0,0 +1,19 @@
+class CloudformationDocs < Formula
+
+  desc "Tool to generate documentation from Cloudformation templates"
+  homepage "https://github.com/daniel-ciaglia/cf-doc"
+  url "https://github.com/daniel-ciaglia/cf-doc/releases/download/0.0.2/terraform-docs-darwin-amd64"
+  sha256 "3e5d000f1691430051fd945c2947535c0f14f0596693b5dd875e457c3adb7d17"
+  version "0.0.2"
+
+  bottle :unneeded
+
+  def install
+    bin.install "cf-doc-darwin-amd64" => "cf-doc"
+  end
+
+  test do
+    system "#{bin}/cf-doc", "--help"
+  end
+
+end
