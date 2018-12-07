b3c86b79b9218c7070b73829396b08b59dec2296
Daniel Ciaglia <daniel.ciaglia@kreuzwerker.de>
2018-12-07 16:40:53 +0100

    finally fix the URL

--- cf-doc.rb
+++ cf-doc.rb
@@ -2,7 +2,7 @@ class CfDoc < Formula
 
   desc "Tool to generate documentation from Cloudformation templates"
   homepage "https://github.com/daniel-ciaglia/cf-doc"
-  url "https://github.com/daniel-ciaglia/cf-doc/releases/download/0.0.2/cf-doc-darwin-amd64"
+  url "https://github.com/daniel-ciaglia/cf-doc/releases/download/v0.0.2/cf-doc-darwin-amd64"
   sha256 "3e5d000f1691430051fd945c2947535c0f14f0596693b5dd875e457c3adb7d17"
   version "0.0.2"
 
